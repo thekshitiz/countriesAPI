f9f5f84446e9ff933b7f8a6381690279edfcb9cd
thekshitiz <dikhwachha@gmail.com>
2025-01-08 13:00:44 +0545

    init seriaizer

--- countries/models.py
+++ countries/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+        
+class Country(models.Model):
+            name = models.CharField(max_length=50, blank=False, default="")
+            capital = models.CharField(max_length=50, blank=False, default="")
+
+class Meta:
+            ordering = ['id']  # Query results sorted by ascending ID
