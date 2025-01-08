cf1cb6448c6bf223510a089f25fc9bf4335fa7e2
thekshitiz <dikhwachha@gmail.com>
2025-01-08 11:26:48 +0545

    added database

--- WorldCountries/settings.py
+++ WorldCountries/settings.py
@@ -77,12 +77,16 @@ WSGI_APPLICATION = 'WorldCountries.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASES = DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',  # Backend for PostgreSQL
+            'NAME': 'countries',                       # Database name
+            'USER': 'postgres',                        # PostgreSQL username
+            'PASSWORD': 'your_password',               # Password
+            'HOST': '127.0.0.1',                       # Localhost
+            'PORT': '5432',                            # Default PostgreSQL port
+        }
     }
-}
 
 
 # Password validation
